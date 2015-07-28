9a819bca417b575bd161d1fcf4e6a84eb66a56e4
Niru Maheswaranathan <nirum@stanford.edu>
2015-07-28 13:38:59 -0700

    Update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.2',
+    version='0.1.3',
 
     description='Pretty ASCII printing of tabular data',
     long_description='''Formatted ASCII printing of tabular data''',

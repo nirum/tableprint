c4250142a10dcf8f3efb3125f58a9c99c08c7060
Niru Maheswaranathan <nirum@stanford.edu>
2015-10-01 00:51:51 -0700

    fixing docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import shlex
+sys.path.append('..')
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the

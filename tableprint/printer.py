94c425be47130719f0a27d179be455ab19385699
Niru Maheswaranathan <niru@fastmail.com>
2020-05-16 11:51:48 -0700

    Updates docstrings.

--- tableprint/printer.py
+++ tableprint/printer.py
@@ -25,14 +25,13 @@ __all__ = ('table', 'header', 'row', 'hrule', 'top', 'bottom', 'banner', 'datafr
 
 # Defaults
 STYLE = 'round'
-WIDTH = None
 FMT = '5g'
 ALIGN = 'right'
 ALIGNMENTS = {"left": "<", "right": ">", "center": "^"}
 
 
 class TableContext:
-    def __init__(self, headers, width=WIDTH, align=ALIGN, style=STYLE, add_hr=True, out=sys.stdout):
+    def __init__(self, headers, width=11, align=ALIGN, style=STYLE, add_hr=True, out=sys.stdout):
         """Context manager for table printing
 
         Parameters
@@ -82,28 +81,28 @@ class TableContext:
         self.out.flush()
 
 
-def table(data, headers=None, format_spec=FMT, width=WIDTH, align=ALIGN, style=STYLE, out=sys.stdout):
+def table(data, headers=None, format_spec=FMT, width=None, align=ALIGN, style=STYLE, out=sys.stdout):
     """Print a table with the given data
 
     Parameters
     ----------
-    data : array_like
+    data: array_like
         An (m x n) array containing the data to print (m rows of n columns)
 
-    headers : list, optional
+    headers: list, optional
         A list of n strings consisting of the header of each of the n columns (Default: None)
 
-    format_spec : string, optional
+    format_spec: string, optional
         Format specification for formatting numbers (Default: '5g')
 
-    width : int or None or array_like, optional
+    width: int or None or array_like, optional
         The width of each column in the table. If None, tries to estimate an appropriate width
-        based on the length of the data in the table. (Default: 11)
+        based on the length of the data in the table. (Default: None)
 
-    align : string
+    align: string
         The alignment to use ('left', 'center', or 'right'). (Default: 'right')
 
-    style : string or tuple, optional
+    style: string or tuple, optional
         A formatting style. (Default: 'fancy_grid')
 
     out: IO writer, optional
@@ -138,18 +137,18 @@ def table(data, headers=None, format_spec=FMT, width=WIDTH, align=ALIGN, style=S
     out.flush()
 
 
-def header(headers, width=WIDTH, align=ALIGN, style=STYLE, add_hr=True):
+def header(headers, width=None, align=ALIGN, style=STYLE, add_hr=True):
     """Returns a formatted row of column header strings
 
     Parameters
     ----------
-    headers : list of strings
+    headers: list of strings
         A list of n strings, the column headers
 
-    width : int
-        The width of each column (Default: 11)
+    width: int
+        The width of each column. If None, automatically determines the width. (Default: None)
 
-    style : string or tuple, optional
+    style: string or tuple, optional
         A formatting style (see STYLES)
 
     Returns
@@ -178,29 +177,29 @@ def header(headers, width=WIDTH, align=ALIGN, style=STYLE, add_hr=True):
     return headerstr
 
 
-def row(values, width=WIDTH, format_spec=FMT, align=ALIGN, style=STYLE):
+def row(values, width=None, format_spec=FMT, align=ALIGN, style=STYLE):
     """Returns a formatted row of data
 
     Parameters
     ----------
-    values : array_like
+    values: array_like
         An iterable array of data (numbers or strings), each value is printed in a separate column
 
-    width : int
-        The width of each column (Default: 11)
+    width: int
+        The width of each column. If None, automatically determines the width. (Default: None)
 
-    format_spec : string
+    format_spec: string
         The precision format string used to format numbers in the values array (Default: '5g')
 
-    align : string
+    align: string
         The alignment to use ('left', 'center', or 'right'). (Default: 'right')
 
-    style : namedtuple, optional
+    style: namedtuple, optional
         A line formatting style
 
     Returns
     -------
-    rowstr : string
+    rowstr: string
         A string consisting of the full row of data to print
     """
     if width is None:
@@ -237,13 +236,13 @@ def hrule(n=1, width=11, linestyle=LineStyle('', '─', '─', '')):
 
     Parameters
     ----------
-    n : int
+    n: int
         The number of columns in the table
 
-    width : int
+    width: int
         The width of each column (Default: 11)
 
-    linestyle : tuple
+    linestyle: tuple
         A LineStyle namedtuple containing the characters for (begin, hr, sep, end).
         (Default: ('', '─', '─', ''))
 
@@ -258,12 +257,12 @@ def hrule(n=1, width=11, linestyle=LineStyle('', '─', '─', '')):
     return linestyle.begin + hrstr + linestyle.end
 
 
-def top(n, width=WIDTH, style=STYLE):
+def top(n, width=11, style=STYLE):
     """Prints the top row of a table"""
     return hrule(n, width, linestyle=STYLES[style].top)
 
 
-def bottom(n, width=WIDTH, style=STYLE):
+def bottom(n, width=11, style=STYLE):
     """Prints the top row of a table"""
     return hrule(n, width, linestyle=STYLES[style].bottom)
 
@@ -273,16 +272,16 @@ def banner(message, width=30, style='banner', out=sys.stdout):
 
     Parameters
     ----------
-    message : string
+    message: string
         The message to print in the banner
 
-    width : int
+    width: int
         The minimum width of the banner (Default: 30)
 
-    style : string
+    style: string
         A line formatting style (Default: 'banner')
 
-    out : writer
+    out: writer
         An object that has write() and flush() methods (Default: sys.stdout)
     """
     out.write(header([message], width=max(width, len(message)), style=style) + '\n')
@@ -294,7 +293,7 @@ def dataframe(df, **kwargs):
 
     Parameters
     ----------
-    df : DataFrame
+    df: DataFrame
         A pandas DataFrame with the table to print
     """
     table(df.values, list(df.columns), **kwargs)
